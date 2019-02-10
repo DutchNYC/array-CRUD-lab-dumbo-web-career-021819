ffc749252eb228263730bd3adb27e7790d304318
Matthew Lee <yleematthew@gmail.com>
2019-02-10 08:19:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array=["Matt", "Nis", "Mom", "Dad"]
-  array.push("Renee")
+  array.push << "Renee"
 end
 
 def add_element_to_start_of_array(array, element)
